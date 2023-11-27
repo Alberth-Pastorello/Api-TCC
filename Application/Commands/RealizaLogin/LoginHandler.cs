c16d36518d106dc7a9cb35fb3820b4da10b34c80
Alberth Clint <89049037+Alberth-Pastorello@users.noreply.github.com>
2023-11-27 19:19:19 -0300

    Update LoginHandler.cs

--- Application/Commands/RealizaLogin/LoginHandler.cs
+++ Application/Commands/RealizaLogin/LoginHandler.cs
@@ -9,24 +9,24 @@ namespace Application.Commands.RealizaLogin
     {
         private CancellationToken _cancellationToken;
         private readonly IMediator _mediator;
-        // private readonly ITokenService _tokenService;
-        // private readonly IUsuarioRepository _usuarioRepository;
-        // private readonly UserManager<IdentityUser> _userManager;
-        // private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ITokenService _tokenService;
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
         public LoginHandler(
             IMediator mediator
-        // ITokenService tokenService,
-        // IUsuarioRepository usuarioRepository,
-        // UserManager<IdentityUser> userManager,
-        // SignInManager<IdentityUser> signInManager
+        ITokenService tokenService,
+        IUsuarioRepository usuarioRepository,
+        UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager
         )
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            // _tokenService = tokenService ?? throw new ArgumentNullException(nameof(tokenService));
-            // _usuarioRepository = usuarioRepository ?? throw new ArgumentNullException(nameof(usuarioRepository));
-            // _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
-            // _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _tokenService = tokenService ?? throw new ArgumentNullException(nameof(tokenService));
+            _usuarioRepository = usuarioRepository ?? throw new ArgumentNullException(nameof(usuarioRepository));
+            _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
         }
 
         public async Task<RespostaLoginDTO> Handle(LoginCommand request, CancellationToken cancellationToken)
@@ -40,55 +40,48 @@ namespace Application.Commands.RealizaLogin
                     await _mediator.Publish(new DomainNotification(key: "Login", value: item), cancellationToken);
                 }
 
-                return new RespostaLoginDTO();
+                return default;
             }
-            // var usuario = await _userManager.FindByEmailAsync(request.Email);
+            var usuario = await _userManager.FindByEmailAsync(request.Email);
 
-            // if (usuario is null)
-            // {
-            //     await _mediator.Publish(new DomainNotification(key: "Login", value: "Usuário ou senha inválidos!"), _cancellationToken);
-            //     return default;
-            // }
+            if (usuario is null)
+            {
+                await _mediator.Publish(new DomainNotification(key: "Login", value: "Usuário ou senha inválidos!"), _cancellationToken);
+                return default;
+            }
+             var loginResult = await _signInManager.PasswordSignInAsync(usuario, command.Senha, true, false);
 
-            // return await RealizarLogin(command: request, usuario);
-            return default;
+            if (!loginResult.Succeeded)
+            {
+                await _mediator.Publish(new DomainNotification(key: "Login", value: "Usuário ou senha inválidos!"), _cancellationToken);
+                return default;
+            }
+
+            var claims = (List<Claim>)await _userManager.GetClaimsAsync(usuario);
+
+            claims.AddRange(new List<Claim>{
+                                new Claim("id", IdentityUser.Id.ToString()),
+                                new Claim("nome", IdentityUser.Nome)
+                            });
+
+            var token = _tokenService.GerarToken(claims);
+            var refreshToken = _tokenService.GerarRefreshToken();
+
+            IdentityUser.AtualizarTokens(token, refreshToken);
+            _usuarioRepository.Atualizar(IdentityUser);
+            var usuarioSalvo = await _usuarioRepository.UnitOfWork.Commit();
+            if (!usuarioSalvo)
+            {
+                await _mediator.Publish(new DomainNotification("LoginHandler", "Não foi possível salvar os tokens do usuário."), _cancellationToken);
+                return default;
+            }
+
+            return new RespostaLoginDTO
+            {
+                Token = token,
+                RefreshToken = refreshToken
+
+            };
         }
-
-        // private async Task<RespostaLoginDTO> RealizarLogin(LoginCommand command, IdentityUser usuario)
-        // {
-        //     var loginResult = await _signInManager.PasswordSignInAsync(usuario, command.Senha, true, false);
-
-        //     if (!loginResult.Succeeded)
-        //     {
-        //         await _mediator.Publish(new DomainNotification(key: "Login", value: "Usuário ou senha inválidos!"), _cancellationToken);
-        //         return default;
-        //     }
-
-        //     var claims = (List<Claim>)await _userManager.GetClaimsAsync(usuario);
-
-        //     claims.AddRange(new List<Claim>{
-        //                         new Claim("id", IdentityUser.Id.ToString()),
-        //                         new Claim("nome", IdentityUser.Nome)
-        //                     });
-
-        //     var token = _tokenService.GerarToken(claims);
-        //     var refreshToken = _tokenService.GerarRefreshToken();
-
-        //     IdentityUser.AtualizarTokens(token, refreshToken);
-        //     _usuarioRepository.Atualizar(IdentityUser);
-        //     var usuarioSalvo = await _usuarioRepository.UnitOfWork.Commit();
-        //     if (!usuarioSalvo)
-        //     {
-        //         await _mediator.Publish(new DomainNotification("LoginHandler", "Não foi possível salvar os tokens do usuário."), _cancellationToken);
-        //         return default;
-        //     }
-
-        //     return new RespostaLoginDTO
-        //     {
-        //         Token = token,
-        //         RefreshToken = refreshToken
-
-        //     };
-        // }
     }
 }
